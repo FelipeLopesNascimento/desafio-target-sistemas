48900245452a1823fad8c08d23de04b1688ff6f8
Felipe Lopes do Nascimento <felipelopesnascimento@hotmail.com>
2024-10-21 15:05:35 -0300

    Desafio concluído

---
+++ Questão 1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Digite um número: ");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int numero))
+{
+    int a = 0, b = 1;
+    while (b <= numero)
+    {
+        if (b == numero)
+        {
+            Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+            return;
+        }
+        int temp = b;
+        b = a + b;
+        a = temp;
+    }
+
+    Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+}
+else
+{
+    Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+}
---
+++ Questão 2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite uma string: ");
+  
+string texto = Console.ReadLine();
+int contador = 0;
+foreach (char c in texto)
+    {
+        if (char.ToLower(c) == 'a')
+        {
+            contador++;
+        }
+    }
+Console.WriteLine($"A letra 'a' aparece {contador} vezes na string.");
